4177ffca9eb87afa84c156ae311a5ee5074d68e4
TomasLegarre <tlegarrematera@udesa.edu.ar>
2022-06-03 17:01:17 -0300

    practicando js

---
+++ js/index.js
@@ -0,0 +1,65 @@
+
+//solamente por PRACTICAR// despues borro todo
+
+
+let info = [{
+    nombre: 'Tomas',
+    apellido: 'Legarre',
+    saludar: function () {
+        return 'hola! soy ' + this.nombre + " " + this.apellido;
+    }
+}
+, //clave para separar los elementos del array// 
+{
+    nombre: 'Ari',
+    apellido: 'Bercovich',
+    saludar: function () {
+        return 'hola! soy ' + this.nombre + " " + this.apellido;
+    }
+}
+]
+
+
+info.push(
+    {
+        nombre: 'Pedro',
+        apellido: 'A 912',
+        saludar: function () {
+            return 'hola! soy ' + this.nombre + " " + this.apellido;
+        }
+    }
+)
+
+//con info.pop puedo quitar el ultimo elemento 
+
+console.log(info[0].saludar()) 
+console.log(info[1].saludar()) 
+console.log(info[2].saludar()) 
+
+
+// prompt --> un cartel que nos da el espacio apra escirbir 
+let mensajeBienvendia = "Hola! Bienvenido ¿Cual es tu nombre?";
+let resultado = prompt(mensajeBienvendia)
+
+let mensaje = "¿Estas seguro que quieres continuar?";
+let resultado2 = confirm(mensaje);
+
+
+
+
+let edad = prompt("¿Cual es tu edad?");
+
+function permiso(edad){
+   
+    let mensaje = ''
+
+if (edad > 18) {
+    mensaje = 'Acceso permitido'
+} 
+else if (edad < 18){
+    mensaje = 'Acceso denegado'
+}
+
+return mensaje
+
+}
