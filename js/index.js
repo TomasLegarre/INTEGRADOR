05b2dfff36c70f2d1ef77434dd5826ccc118b48e
TomasLegarre <tlegarrematera@udesa.edu.ar>
2022-06-10 11:54:59 -0300

    kaka

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ fetch ('https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/tracks
       console.log (info)
 
          let section2 = ''
-         for (let i = 0; i < 4; i++){ 
+         for (let i = 0; i < 5; i++){ 
 
        section2 += ` <article class="cajas-canciones">
         <img src=${info.data[i].artist.picture} alt="" class="imagenC bohemian"> 
@@ -53,3 +53,31 @@ section.innerHTML = section2;
   console.log ('el error fue: ' + error);
 })
 
+
+
+
+
+let artistas1 = document.querySelector ('.artistas') 
+
+fetch ('https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/artists')
+      .then(function(datos) {
+        return datos.json();
+      })
+
+      .then(function(info){
+        console.log (info)
+  
+           let artistas2  = ''
+           for (let i = 0; i < 4; i++){
+  
+         artistas2 += ` <section class="artistas">
+            <article class="cajas-artistas">
+            <img src="${info.data[i].picture}" alt="" class="imagenAr">
+            <a href="./detail-artist.html?id=${info.data[i].id}" class="linkartista"> 
+            <br><br> ${info.data[i].name} </a>
+            </article> ` 
+  
+  artistas1.innerHTML = artistas2; 
+  }
+  
+  })  
