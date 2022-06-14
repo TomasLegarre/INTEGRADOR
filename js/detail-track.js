7cc707f9fa58494547a5a1f90a6d2367bb3f040b
TomasLegarre <tlegarrematera@udesa.edu.ar>
2022-06-14 10:01:55 -0300

    caca

--- js/detail-track.js
+++ js/detail-track.js
@@ -21,3 +21,39 @@ console.log('error')
 
 
 )
+
+let cacniones1 = document.querySelector ('.trackdetails')
+
+
+fetch ('https://api.allorigins.win/raw?url=https://api.deezer.com/chart/0/tracks')
+  
+  .then (function(detalle) {
+  return detalle.json();
+  })
+
+  .then (function(tracks){
+    console.log (tracks)
+  
+  let canciones2 = ''
+  for (let i = 0; i < 5; i++){
+    
+    canciones2 += `
+    <article class="trackbox">
+    <img src="${tracks.data[i].picture_small}" alt="fotoboh" class="fotobohemian track">
+    <a href="./detail-artist.html" class="nombretema"><br> ${tracks.data[i].title} </a>
+    <br>
+    <a href="./detail-album.html?=id=${tracks.data[i].id}" class="albumde"><br> ${tracks.data[i].album}</a>
+    <br>
+    <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3z8h0TU7ReDPLIbEnYhWZb?utm_source=generator&theme=0" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+    <br>
+    <br>
+    <button class="boton">+ Agregar a mi Playlist</button>
+</article>
+    
+    
+    `
+  cacniones1.innerHTML = canciones2;
+  
+  }
+
+  })
