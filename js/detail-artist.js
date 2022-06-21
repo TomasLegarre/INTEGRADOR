60e290adab65975638caa6f559227324206c6ee6
arielbercovich <abercovich@udesa.edu.ar>
2022-06-21 10:18:57 -0300

    fetch2

--- js/detail-artist.js
+++ js/detail-artist.js
@@ -28,7 +28,6 @@ let idArtista = qsToObject.get ('id');
 
 let artistas1 = document.querySelector ('.artistdetails')
 
-// let urlArtistas = `https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/${idArtista}`;
 fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/artist/' + idArtista)
   .then (function(detalle) {
   return detalle.json();
